008617299b10ff615b06dfb8a46471a8b3da82a6
Petersen, Nicholas <petersen.nicholas@principal.com>
2020-03-11 19:15:39 -0500

    Added guide

---
+++ data.js
@@ -0,0 +1,32 @@
+let primaryResidencePathData = [
+	{
+		id: "pr0",
+		question: "Do you currently own your Primary Residence",
+		answer1: "Yes",
+		answer2: "No"
+	},
+	{
+		id: "pr1",
+		question: "Do you intend to sell your Primary Residence prior to the purchase of your new Primary Residence?",
+		answer1: "Yes",
+		answer2: "No"
+	},
+	{
+		id: "pr2",
+		question: "Do you intend to sell your Primary Residence prior to the purchase of your new Primary Residence?",
+		answer1: "Yes",
+		answer2: "No"
+	},
+	{
+		id: "pr3",
+		question: "Do you intend to build your new home on privately owned land or on land you will lease/rent?",
+		answer1: "Privately Owned",
+		answer2: "Lease/Rent"
+	},
+	{
+		id: "pr4",
+		question: "Do you currently own the land you will build your new home or will you be purchasing the land/home at the same time?",
+		answer1: "Privately Owned",
+		answer2: "Lease/Rent"
+	}
+];
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {	
-	
 	// Random Alert shown for the fun of it
 	function randomAlert() {
 		var min = 5,
@@ -14,91 +13,26 @@ $(document).ready(function() {
 });
 
 $('.btn').click(function(event) {
-    event.preventDefault();
-    var target = $(this).data('target');
+		event.preventDefault();
+		var target = $(this).data('target');
 	// console.log('#'+target);
 	$('#click-alert').html('data-target= ' + target).fadeIn(50).delay(3000).fadeOut(1000);
 	
 });
 
-// Multi-Step Form
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the crurrent tab
-
-function showTab(n) {
-  // This function will display the specified tab of the form...
-  var tab = document.getElementsByClassName("tab");
-  tab[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-  if (n == (tab.length - 1) && n != 1) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
-  //... and run a function that will display the correct step indicator:
-  fixStepIndicator(n)
-}
-
-function nextPrev(n) {
-  // This function will figure out which tab to display
-  var tab = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
-  tab[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form...
-  if (currentTab >= tab.length) {
-    // ... the form gets submitted:
-    document.getElementById("regForm").submit();
-    return false;
-  }
-  // Otherwise, display the correct tab:
-  showTab(currentTab);
-}
-
-function validateForm() {
-  // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false
-      valid = false;
-    }
-  }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
-}
-
-function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i, x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  //... and adds the "active" class on the current step:
-  x[n].className += " active";
-}
-
-var pathSelection = document.querySelectorAll(".path-selector");
-
+var pathSelection = document.querySelectorAll(".js-path-selector");
 for (i = 0; i < pathSelection.length; i++) {
-  pathSelection[i].addEventListener("click", servePath);
+	pathSelection[i].addEventListener("click", servePath);
+}
+
+// var pathTrigger = document.querySelectorAll(".js-path-trigger");
+// for (i = 0; i < pathTrigger.length; i++) {
+// 	pathTrigger[i].addEventListener("click", guide);
+// }
+
+var pathAnswer = document.querySelectorAll(".js-path-answer");
+for (i = 0; i < pathAnswer.length; i++) {
+	pathAnswer[i].addEventListener("click", guide);
 }
 
 var primaryResidencePath = document.getElementById('primaryResidencePath');
@@ -109,61 +43,160 @@ var primaryResidenceTabs = document.getElementsByClassName('primary-residence-ch
 var secondVacationHomeTabs = document.getElementsByClassName('second-vacation-home-child');
 var investmentPropertyTabs =  document.getElementsByClassName('investment-property-child');
 
-function servePath() {
+// Multi-Step Form
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the crurrent tab
 
-  // On click reset all
-  resetTabs();
+function showTab(n) {
+	// This function will display the specified tab of the form...
+	var tab = document.getElementsByClassName("tab");
+	tab[n].style.display = "block";
+	//... and fix the Previous/Next buttons:
+	if (n == 0) {
+		document.getElementById("prevBtn").style.display = "none";
+	} else {
+		document.getElementById("prevBtn").style.display = "inline";
+	}
+	if (n == (tab.length - 1) && n != 1) {
+		document.getElementById("nextBtn").innerHTML = "Submit";
+	} else {
+		document.getElementById("nextBtn").innerHTML = "Next";
+	}
+	//... and run a function that will display the correct step indicator:
+	fixStepIndicator(n)
+}
 
-  if (this.id == 'primaryResidence') {
-    primaryResidencePath.style.display = 'block';
-    for (i = 0; primaryResidenceTabs.length; i++) {
-      if (!primaryResidenceTabs[i].classList.contains('tab')) {
-        primaryResidenceTabs[i].classList.add('tab');
-      }
-    }
-  }
+function nextPrev(n) {
+	// This function will figure out which tab to display
+	var tab = document.getElementsByClassName("tab");
+	// Exit the function if any field in the current tab is invalid:
+	if (n == 1 && !validateForm()) return false;
+	// Hide the current tab:
+	tab[currentTab].style.display = "none";
+	// Increase or decrease the current tab by 1:
+	currentTab = currentTab + n;
+	// if you have reached the end of the form...
+	if (currentTab >= tab.length) {
+		// ... the form gets submitted:
+		document.getElementById("regForm").submit();
+		return false;
+	}
+	// Otherwise, display the correct tab:
+	showTab(currentTab);
+}
 
-  if (this.id == 'secondVacationHome') {
-    secondVacationHomePath.style.display = 'block';
-    for (i = 0; secondVacationHomeTabs.length; i++) {
-      if (!secondVacationHomeTabs[i].classList.contains('tab')) {
-        secondVacationHomeTabs[i].classList.add('tab');
-      }
-    }
-  }
+function validateForm() {
+	// This function deals with validation of the form fields
+	var x, y, i, valid = true;
+	x = document.getElementsByClassName("tab");
+	y = x[currentTab].getElementsByTagName("input");
+	// A loop that checks every input field in the current tab:
+	for (i = 0; i < y.length; i++) {
+		// If a field is empty...
+		if (y[i].value == "") {
+			// add an "invalid" class to the field:
+			y[i].className += " invalid";
+			// and set the current valid status to false
+			valid = false;
+		}
+	}
+	// If the valid status is true, mark the step as finished and valid:
+	if (valid) {
+		document.getElementsByClassName("step")[currentTab].className += " finish";
+	}
+	return valid; // return the valid status
+}
 
-  if (this.id == 'investmentProperty') {
-    investmentPropertyPath.style.display = 'block';
-    for (i = 0; investmentPropertyTabs.length; i++) {
-      if (!investmentPropertyTabs[i].classList.contains('tab')) {
-        investmentPropertyTabs[i].classList.add('tab');
-      }
-    }
-  }
-  
+function fixStepIndicator(n) {
+	// This function removes the "active" class of all steps...
+	var i, x = document.getElementsByClassName("step");
+	for (i = 0; i < x.length; i++) {
+		x[i].className = x[i].className.replace(" active", "");
+	}
+	//... and adds the "active" class on the current step:
+	x[n].className += " active";
 }
 
 function resetTabs() {
 
-  primaryResidencePath.style.display = 'none';
-  secondVacationHomePath.style.display = 'none';
-  investmentPropertyPath.style.display = 'none';
+	primaryResidencePath.style.display = 'none';
+	secondVacationHomePath.style.display = 'none';
+	investmentPropertyPath.style.display = 'none';
 
-  for (i = 0; i < primaryResidenceTabs.length; i++) {
-    if (primaryResidenceTabs[i].classList.contains('tab')) {
-      primaryResidenceTabs[i].classList.remove('tab');
-    }
-  }
+	for (i = 0; i < primaryResidenceTabs.length; i++) {
+		if (primaryResidenceTabs[i].classList.contains('tab')) {
+			primaryResidenceTabs[i].classList.remove('tab');
+		}
+	}
 
-  for (i = 0; i < secondVacationHomeTabs.length; i++) {
-    if (secondVacationHomeTabs[i].classList.contains('tab')) {
-      secondVacationHomeTabs[i].classList.remove('tab');
-    }
-  }
+	for (i = 0; i < secondVacationHomeTabs.length; i++) {
+		if (secondVacationHomeTabs[i].classList.contains('tab')) {
+			secondVacationHomeTabs[i].classList.remove('tab');
+		}
+	}
 
-  for (i = 0; i < investmentPropertyTabs.length; i++) {
-    if (investmentPropertyTabs[i].classList.contains('tab')) {
-      investmentPropertyTabs[i].classList.remove('tab');
-    }
-  }
+	for (i = 0; i < investmentPropertyTabs.length; i++) {
+		if (investmentPropertyTabs[i].classList.contains('tab')) {
+			investmentPropertyTabs[i].classList.remove('tab');
+		}
+	}
+};
+
+function servePath() {
+
+	// On click reset all
+	resetTabs();
+
+	if (this.id == 'primaryResidence') {
+		primaryResidencePath.style.display = 'block';
+		for (i = 0; primaryResidenceTabs.length; i++) {
+			if (!primaryResidenceTabs[i].classList.contains('tab')) {
+				primaryResidenceTabs[i].classList.add('tab');
+			}
+			if (primaryResidenceTabs[i] !== 0) {
+				primaryResidenceTabs[i].style.display = "none";
+			}
+		}
+	}
+
+	if (this.id == 'secondVacationHome') {
+		secondVacationHomePath.style.display = 'block';
+		for (i = 0; secondVacationHomeTabs.length; i++) {
+			if (!secondVacationHomeTabs[i].classList.contains('tab')) {
+				secondVacationHomeTabs[i].classList.add('tab');
+			}
+		}
+	}
+
+	if (this.id == 'investmentProperty') {
+		investmentPropertyPath.style.display = 'block';
+		for (i = 0; investmentPropertyTabs.length; i++) {
+			if (!investmentPropertyTabs[i].classList.contains('tab')) {
+				investmentPropertyTabs[i].classList.add('tab');
+			}
+		}
+	}
+	
+}
+
+function getQuestion() {
+	var question = this.parentElement.childNodes[1].innerHTML;
+	console.log(question);
+}
+
+function guide() {
+
+	var question = "Hello?";
+	var answer1 = "Yes";
+	var answer2 = "No";
+
+	var tab = 
+	'<div class="primary-residence-child">' + 
+	'<p class="tab-question">'+ question +'</p>' +
+	'<div class="btn-group js-path-trigger" data-toggle="buttons" style="text-align: center;">' +
+		'<label id="pr1a" class="btn btn-primary js-path-answer"><input class="radio-input" type="radio" name="options">'+ answer1 +'</label>' +
+		'<label id="pr1b" class="btn btn-primary js-path-answer"><input class="radio-input" type="radio" name="options">'+ answer2 +'</label>' +
+	'</div></div>';
+	console.log(tab);
+
 }
